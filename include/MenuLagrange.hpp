7e4eea1d46171b2b51669bf4ef66a25c9247ecd5
ELIASLOPEZGOMEZ <143851923+ELIASLOPEZGOMEZ@users.noreply.github.com>
2023-11-30 22:51:02 -0600

    2 clases 30/11
    agregamos 2 clases mas

--- include/MenuLagrange.hpp
+++ include/MenuLagrange.hpp
@@ -1,15 +1,74 @@
-#ifndef MENULAGRANGE_H
-#define MENULAGRANGE_H
-
 #include <iostream>
 #include <iomanip>
 #include <conio.h>
+using namespace std;
+#include "Lagrange.hpp"
 
 class MenuLagrange
 {
 private:
-    /* data */
+    int opcion;
+    int err;
+
 public:
-    MenuLagrange(/* args */) {}
-    ~MenuLagrange() {}
+    void showMenuLagrange();
 };
+
+void MenuLagrange::showMenuLagrange()
+{
+   
+    Lagrange funciones;
+    do
+    {
+        system("cls");
+        cout << "\n1- INGRESO DE DATOS\n";
+        cout << "2- CALCULO LX\n";
+        cout << "3- CALCULO POLINOMIO\n";
+        cout << "4- Vuelta a menu principal\n";
+
+        cout << "\n\n";
+
+        cout << "Ingrese una opcion: ";
+        cin >> opcion;
+
+        switch (opcion)
+        {
+        case 1:
+            system("cls");
+            do
+            {
+             err = funciones.setValores();
+            } while (err);
+            funciones.setContadores();
+            cout << endl;
+            cout << "Presione una tecla para volver al menu" << endl;
+            while (!kbhit())
+                ;
+            break;
+        case 2:
+            system("cls");
+            funciones.calcularLx();
+            cout << endl;
+            cout << "Presione una tecla para volver al menu" << endl;
+            while (!kbhit())
+                ;
+            break;
+        case 3:
+            system("cls");
+            funciones.calcularPolinomio();
+            cout << endl;
+            cout << "Presione una tecla para volver al menu principal" << endl;
+            while (!kbhit())
+                ;
+            return;
+        case 4:
+            system("cls");
+            // cout<<"Usted esta regresando al menu principal.\n";
+            break;
+        default:
+            system("cls");
+            cout << "Ha ingresado una opcion incorrecta. Elija nuevamente una opcion.\n";
+            break;
+        }
+    } while (opcion != 4);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,62 @@
-#include "Gauss.hpp"
-#include "Lagrange.hpp"
-#include "gauss.cpp"
-#include "lagrange.cpp"
-#include "menugauss.cpp"
-#include "menulagrange.cpp"
+
+
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+using namespace std;
+#include "../include/MenuLagrange.hpp"
+
 
 void menu();
 
-int main (){
+int main()
+{
 
-	menu();
+    menu();
 
-return 0;
+    return 0;
 }
 
+void menu()
+{
+    int opcion;
 
-void menu(){
-   int opcion;
-
-   do{
+    do
+    {
         system("cls");
-        cout<<"/// MENU DE RESOLUCIONES DE ECUACIONES";
-        cout<<"\n\n\n";
+        cout << "/// MENU DE RESOLUCIONES DE ECUACIONES";
+        cout << "\n\n\n";
 
-        cout<<"1- METODO DE RESOLUCION DE LAGRANGE\n";
-        cout<<"2- METODO DE RESOLUCION DE SISTEMAS DE ECUACIONES\n";
-        cout<<"3- SALIDA\n\n";
+        cout << "1- METODO DE RESOLUCION DE LAGRANGE\n";
+        cout << "2- METODO DE RESOLUCION DE SISTEMAS DE ECUACIONES\n";
+        cout << "3- SALIDA\n\n";
 
-        cout<<"Ingrese una opcion: ";
-        cin>>opcion;
+        cout << "Ingrese una opcion: ";
+        cin >> opcion;
 
-        switch(opcion){
-            case 1:
-                system("cls");
-                cout<<"METODO DE RESOLUCION DE LAGRANGE \n";
-                menuLagrange();
-                //system("pause");
-                break;
-            case 2:
-                system("cls");
-                cout<<"METODO DE RESOLUCION DE SISTEMAS DE ECUACIONES \n";
-                menuGauss();
-                //system("pause");
-                break;
-            case 3:
-                system("cls");
-                cout<<"Usted esta saliendo del programa. Hasta pronto.\n";
-                break;
-            default:
-                system("cls");
-                cout<<"Ha ingresado una opcion incorrecta. Elija nuevamente una opcion.\n";
-                break;
+        switch (opcion)
+        {
+        case 1:
+            system("cls");
+            cout << "METODO DE RESOLUCION DE LAGRANGE \n";
+            MenuLagrange menulag;
+            menulag.showMenuLagrange();
+            // system("pause");
+            break;
+        case 2:
+            system("cls");
+            cout << "METODO DE RESOLUCION DE SISTEMAS DE ECUACIONES \n";
+            // menuGauss();
+            // system("pause");
+            break;
+        case 3:
+            system("cls");
+            cout << "Usted esta saliendo del programa. Hasta pronto.\n";
+            break;
+        default:
+            system("cls");
+            cout << "Ha ingresado una opcion incorrecta. Elija nuevamente una opcion.\n";
+            break;
         }
-   }while(opcion!=3);
+    } while (opcion != 3);
 }
